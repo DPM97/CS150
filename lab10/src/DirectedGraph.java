3b86edaf2ccc2c30f3663b2048927ced01dcf466
DPM97 <34420038+DPM97@users.noreply.github.com>
2019-11-13 14:56:57 -0500

    f

---
+++ lab10/src/DirectedGraph.java
@@ -0,0 +1,180 @@
+import java.util.ArrayList;
+
+/**
+ * graph class
+ * @param <E> generic
+ */
+
+public class DirectedGraph <E> {
+    /**
+     * arraylist of nodes
+     */
+    ArrayList<DirectedGraphNode> nodes;
+
+    /**
+     * constructor
+     */
+
+    public DirectedGraph() {
+        this.nodes = new ArrayList<>();
+    }
+
+    /**
+     * adds a node to the graph
+     * @param val value being added
+     * @return true if successful
+     */
+
+    public boolean addNode(E val) {
+        for (DirectedGraphNode node : this.nodes) {
+            if (node.key == val) {
+                return false;
+            }
+        }
+        this.nodes.add(new DirectedGraphNode(val));
+        return true;
+
+    }
+
+    /**
+     * add an edge to a node
+     * @param val1 first node's value
+     * @param val2 second node's value
+     * @param weight weight of the edge
+     * @return true if successful
+     */
+
+    public boolean addEdge(E val1, E val2, int weight) {
+        DirectedGraphNode node1 = null;
+        DirectedGraphNode node2 = null;
+        for (DirectedGraphNode node : this.nodes) {
+            if (node.key.equals(val1)) {
+                node1 = node;
+            }
+        }
+        for (DirectedGraphNode node : this.nodes) {
+            if (node.key.equals(val2)) {
+                node2 = node;
+            }
+        }
+
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+
+        for (DirectedGraphEdge edge : node1.edges) {
+            if (edge.start.equals(node1) && edge.end.equals(node2)) {
+                edge.weight = weight;
+                return true;
+            }
+        }
+
+        DirectedGraphEdge edge = new DirectedGraphEdge(node1, node2, weight);
+        if (node1.edges.add(edge)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * gets neighbors of a node
+     * @param val key value of the node in question
+     * @return arraylist of node keys
+     */
+
+    public ArrayList<E> getNeighbors(E val) {
+        ArrayList<E> list = new ArrayList<>();
+        DirectedGraphNode node = null;
+        for (DirectedGraphNode nodes : this.nodes) {
+            if (val.equals(nodes.key)) {
+                node = nodes;
+            }
+        }
+        if (node == null) {
+            return null;
+        }
+
+        for (DirectedGraphEdge edge : node.edges) {
+            list.add(edge.end.key);
+        }
+        return list;
+    }
+
+    /**
+     * runs dijkstra's algorithm on a node for all other reachable nodes
+     * @param val key value of node in question
+     */
+
+    public void dijkstra(E val) {
+        DirectedGraphNode node = null;
+        for (DirectedGraphNode nodes : this.nodes) {
+            if (nodes.key == val) {
+                node = nodes;
+            }
+        }
+        if (node == null) {
+            return;
+        }
+
+        ArrayList<E> neighbors = getNeighbors(val);
+
+    }
+
+    /**
+     * graph node class
+     */
+
+    public class DirectedGraphNode {
+        /**
+         * key
+         */
+        E key;
+        /**
+         * list of node's edges
+         */
+        ArrayList<DirectedGraphEdge> edges;
+
+        /**
+         * constructor
+         * @param data key
+         */
+
+        public DirectedGraphNode(E data) {
+            this.key = data;
+            this.edges = new ArrayList<>();
+        }
+    }
+
+    /**
+     * edge class
+     */
+
+    public class DirectedGraphEdge {
+        /**
+         * start node
+         */
+        DirectedGraphNode start;
+        /**
+         * end ndoe
+         */
+        DirectedGraphNode end;
+        /**
+         * edge weight
+         */
+        int weight;
+
+        /**
+         * constructor
+         * @param start start node
+         * @param end end node
+         * @param weight edge weight
+         */
+
+        public DirectedGraphEdge(DirectedGraphNode start, DirectedGraphNode end, int weight) {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+    }
+}
---
+++ lab10/src/GraphExperiment.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.Arrays;
+
+/**
+ * experiment class
+ */
+
+public class GraphExperiment {
+    /**
+     * main method
+     * @param args input args (not needed)
+     * @throws IOException exception for file reader
+     */
+    public static void main(String[] args) throws IOException {
+        GraphExperiment exp = new GraphExperiment();
+        exp.start();
+    }
+
+    /**
+     * start method
+     * @throws IOException exception for file reader
+     */
+
+    public void start() throws IOException {
+        DirectedGraph<String> graph = new DirectedGraph<String>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("./src/graph.txt")));
+        String line = reader.readLine();
+        String[] split = line.split(" ");
+        for (int i = 0; i < split.length; i++) { //add nodes
+            graph.addNode(split[i]);
+        }
+
+        line = reader.readLine();
+        while(line != null) {
+            split = line.split(" ");
+            System.out.println(Arrays.toString(split));
+            graph.addEdge(split[0], split[1], Integer.parseInt(split[2]));
+            line = reader.readLine();
+        }
+
+        System.out.println(graph.getNeighbors("a"));
+    }
+}
